8912ab6bb0ab9257ecdbbfb4b5347adae838a9d4
josanche <jose.sanchez@mlb.com>
2015-08-24 12:36:10 -0400

    Check for environment variable created by docker link to get correct host address

--- routes/config.js
+++ routes/config.js
@@ -1,7 +1,7 @@
 var config = {},
     isDev = process.env.NODE_ENV == 'development';
 
-config.SPORTIFY_SERVICE_HOST = isDev ? 'localhost' : 'sportify-app.herokuapp.com';
+config.SPORTIFY_SERVICE_HOST = isDev ? (process.env.SPORTIFY_1_PORT_9000_TCP_ADDR || 'localhost') : 'sportify-app.herokuapp.com';
 config.SPORTIFY_SERVICE_PORT = '9000';
 
 module.exports = config;
