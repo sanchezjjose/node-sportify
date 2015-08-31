e9a578e45678541375ebb5150b3bf72df1e68431
josanche <jose.sanchez@mlb.com>
2015-08-31 12:55:56 -0400

    Changing sportify service port to 80 for production

--- routes/config.js
+++ routes/config.js
@@ -2,6 +2,6 @@ var config = {},
     isDev = process.env.NODE_ENV == 'development';
 
 config.SPORTIFY_SERVICE_HOST = isDev ? (process.env.SPORTIFY_1_PORT_9000_TCP_ADDR || 'localhost') : 'sportify-app.herokuapp.com';
-config.SPORTIFY_SERVICE_PORT = '9000';
+config.SPORTIFY_SERVICE_PORT = isDev ? '9000' : '80';
 
 module.exports = config;
