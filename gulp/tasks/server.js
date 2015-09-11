76697f36ea09a7a32d5a6df9ccd5f7332212e638
josanche <jose.sanchez@mlb.com>
2015-09-10 21:16:58 -0400

    Upgraded gulp-live-server

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -2,23 +2,25 @@ var gulp = require('gulp');
 var gls = require('gulp-live-server');
 
 gulp.task('server', function() {
-  var server = gls('bin/www', { env: { NODE_ENV: 'development' } });
+  var server = gls('bin/www', { env: { NODE_ENV: 'development' }});
   server.start();
-    
-  gulp.watch(['public/images/*',
+  
+  gulp.watch(['app.js',
+              'routes/*.js',
               'public/javascripts/*.js',
   	          'public/stylesheets/*.less',
-              'routes/*.js',
               'views/*.hbs',
-              'views/*/*.hbs',
-  	          'app.js'],
-  	          server.notify);
+              'views/*/*.hbs'
+              ],
+              function (file) {
+                server.notify.apply(server, [file]);
+              });
 
   // run the js and less gulp tasks to rebuild bundles
   gulp.watch("public/stylesheets/*.less", ['less']);
   gulp.watch("public/javascripts/*.js", ['js']);
 
   // restart if server side files are changed
-  gulp.watch('app.js', server.start);
-  gulp.watch('routes/*.js', server.start);
+  gulp.watch('app.js', function() { server.start() });
+  gulp.watch('routes/*.js', function() { server.start() });
 });
